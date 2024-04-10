8ccc37b690aebd2a7c405814d110f5ba54b57cbc
Alan Berg <Alan.Berg@Acoustic.co>
2024-04-11 01:52:40 +0200

    ALL DONE

--- Zad2/app/src/main/java/com/example/myapplication/RecipiesFragment.java
+++ Zad2/app/src/main/java/com/example/myapplication/RecipiesFragment.java
@@ -2,11 +2,15 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,5 +64,12 @@ public class RecipiesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_recipies, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        TextView recipesContext = getView().findViewById(R.id.recipesContent);
+        recipesContext.setMovementMethod(new ScrollingMovementMethod());
+        super.onViewCreated(view, savedInstanceState);
     }
 }
