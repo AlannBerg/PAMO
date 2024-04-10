1203197c9d7558ef9c250a8214e6516fb37223bb
Alan Berg <Alan.Berg@Acoustic.co>
2024-04-11 00:11:51 +0200

    Navbar works well

--- Zad2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Zad2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,14 +2,12 @@ package com.example.myapplication;
 
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
 import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,19 +19,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
         binding.bottomNavigationView.setOnItemReselectedListener(item -> {
-            if (item.getItemId() == R.id.main) {
-
+            if (item.getItemId() == R.id.home) {
+                replaceFragment(new HomeFragment());
             }
             if (item.getItemId() == R.id.bmi) {
-
+                replaceFragment(new BmiFragment());
             }
             if (item.getItemId() == R.id.kcal) {
-
+                replaceFragment(new KcalFragment());
             }
-            if (item.getItemId() == R.id.recipies) {
-
+            if (item.getItemId() == R.id.recipes) {
+                replaceFragment(new RecipiesFragment());
             }
 
         });
