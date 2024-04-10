1694282e2f05e4a3eb55f7bc01040310e5df278f
Alan Berg <Alan.Berg@Acoustic.co>
2024-04-10 23:54:31 +0200

    Cleared main layout

--- Zad2/app/src/main/java/com/example/myapplication/BmiFragment.java
+++ Zad2/app/src/main/java/com/example/myapplication/BmiFragment.java
@@ -1,4 +1,3 @@
-
 package com.example.myapplication;
 
 import android.os.Bundle;
@@ -63,64 +62,3 @@ public class BmiFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_bmi, container, false);
     }
 }
-
-
-/**
- *   private TextView userWeightInput;
- *     private TextView userHeightInput;
- *     private TextView userBMIOutput;
- *
- *     private Button calculateButton;
- *
- *     private Integer bmiValue;
- *
- *     @Override
- *     protected void onCreate(Bundle savedInstanceState) {
- *         super.onCreate(savedInstanceState);
- *         EdgeToEdge.enable(this);
- *         setContentView(R.layout.activity_main);
- *
- *         userWeightInput = (TextView) findViewById(R.id.weightEditText);
- *         userHeightInput = (TextView) findViewById(R.id.heightEditText);
- *         userBMIOutput = (TextView) findViewById(R.id.BMIValueText);
- *
- *         calculateButton = (Button) findViewById(R.id.calculateBMIButton);
- *         calculateButton.setOnClickListener(summitButtonListener);
- *
- *         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
- *             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
- *             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
- *             return insets;
- *         });
- *     }
- *
- *     private final View.OnClickListener summitButtonListener =
- *             new View.OnClickListener() {
- *                 @Override
- *                 public void onClick(View v) {
- *                     Integer userHeight = getInteger(userHeightInput);
- *                     Integer userWeight = getInteger(userWeightInput);
- *                     if (nonNull(userWeight) && nonNull(userHeight)) {
- *                         calculateBMI(userHeight, userWeight);
- *                     }
- *                 }
- *             };
- *
- *     private void calculateBMI(@NotNull Integer userHeightCm, @NotNull Integer userWeight) {
- *         double userHeightMeters = userHeightCm / 100.0;
- *         Double userHeightMetersSquare = Math.pow(userHeightMeters,2);
- *
- *         Double bmiValue = userWeight / userHeightMetersSquare;
- *         String bmiValueString = String.format("%.1f", bmiValue);
- *         userBMIOutput.setText(bmiValueString);
- *     }
- *
- *     private Integer getInteger(@NotNull TextView textView) {
- *         try {
- *             return Integer.valueOf(textView.getText().toString());
- *         } catch (Exception e) {
- *             return null;
- *         }
- *     }
- * }
- */
