76e7a6328f81e852d70e25233095c4359a714155
AlanBAcoustic <alan.berg@acoustic.co>
2024-05-06 19:23:20 +0200

    Lab4 added kotlin

---
+++ Zad4/kotlin_lab4/src/main/kotlin/com/example/kotlin_lab4/First.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.kotlin
+
+class First {
+
+    fun main() {
+        val name = "Mary"
+        val age = 20
+        println("$name is $age years old")
+    }
+}
---
+++ Zad4/kotlin_lab4/src/test/kotlin/com/example/kotlin_lab4/KotlinLab4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin_lab4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinLab4ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
